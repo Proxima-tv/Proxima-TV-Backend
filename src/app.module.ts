78da484ab881ce9c279f4280f6059b104c60f294
Steev <mauricefl@outlook.de>
2022-01-22 14:15:26 +0100

    added crypto placeholders

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VideosController } from './videos/videos.controller';
+import { CryptoService } from './crypto/crypto.service';
 
 @Module({
   imports: [],
   controllers: [AppController, VideosController],
-  providers: [AppService],
+  providers: [AppService, CryptoService],
 })
 export class AppModule {}
---
+++ src/crypto/crypto.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CryptoService {
+    encrypt(): string {
+        return 'null';
+    }
+    decrypt(): string {
+        return 'null';
+    }
+}
