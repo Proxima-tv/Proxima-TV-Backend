4e5690189e07a7f951ac2fec0a54070aa79a4933
Steev <mauricefl@outlook.de>
2022-02-07 20:23:43 +0100

    major stabillity and api changes deprecated functions that will either be removed or changed fixed crashes crashing the server when no author is given in comments and when no correct video id was given implemented update functions in all services properly added missing entity properties on all data entities removed video from uploads implemented deletion of videos of both file and database entrie

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -8,8 +8,14 @@ export class UserService {
     constructor(@InjectRepository(User)
     private userRepository: Repository<User>){}
 
+    /**
+     * creates a user
+     * @param user the user object as defined by User Entity Model
+     * @returns true if successfull false if not
+     */
     async createUser(user:User): Promise<boolean>{
         try {
+            user["profile_id"] = Math.floor(Math.random() * 999999);
             await this.userRepository.save(user);
             return true;
         } catch (error) {
@@ -18,6 +24,11 @@ export class UserService {
         }
     }
 
+    /**
+     * gets a user identified by its registered email
+     * @param _email the email to look for
+     * @returns the object with user data or null if erroring
+     */
     getUser(_email:string):Object | null{
         return this.userRepository.find({
             select:["id", "email","name","profile_likes","profile_pic","username"],
@@ -25,6 +36,11 @@ export class UserService {
         });
     }
 
+    /**
+     * gets a user by its user_id
+     * @param _id the user id to look for
+     * @returns the object with user data or null if erroring
+     */
     getUserById(_id:number):Object | null{
         return this.userRepository.find({
             select:["id", "email","name","profile_likes","profile_pic","username"],
@@ -32,6 +48,12 @@ export class UserService {
         });
     }
 
+    /**
+     * fetches userdata by the username
+     * @param _username the username used as identifier
+     * @deprecated this function will be removed in favor if the userid function
+     * @returns the object with user data or null if erroring
+     */
     getUserbyName(_username:string):Object | null{
         return this.userRepository.find({
             select:["id", "email","name","profile_likes","profile_pic","username"],
@@ -39,9 +61,15 @@ export class UserService {
         }); 
     }
 
-    async updateUser(user:User): Promise<Boolean>{
+    /**
+     * updates a user with specific data
+     * @param _id the user id for the user to update
+     * @param _changes the changes as json object
+     * @returns true if changes were applied false if error occured
+     */
+    async updateUser(_id:number, _changes:object): Promise<Boolean>{
         try {
-            this.userRepository.update(user.id, user);
+            this.userRepository.update(_id, _changes);
             return true;
         } catch (error) {
             console.log(error);
@@ -49,6 +77,12 @@ export class UserService {
         }
     }
 
+    /**
+     * deletes a user
+     * @param user the users full dataobject
+     * @deprecated this will be replaced by a better solution implementation of this will stop working in future
+     * @returns true if removal was successfull false if errors occured
+     */
     async deleteUser(user:User): Promise<boolean>{
         try {
             this.userRepository.delete(user);
@@ -59,6 +93,12 @@ export class UserService {
         }
     }
 
+    /**
+     * veryfies the users login attempt
+     * @param _email the email used as identifier 
+     * @param _password the password to validate
+     * @returns true if user values are correct false if not
+     */
     async verifyPassword(_email:string, _password):Promise<boolean> {
         console.log("email: " + _email);
         console.log(_password);
